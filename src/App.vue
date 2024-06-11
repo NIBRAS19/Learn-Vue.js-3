088637ddeee2a83f6b7b8b0827b85f205181990c
NIBRAS19 <mhd.nibras.19@gmail.com>
2024-06-11 22:02:15 +0530

    learn-vue.js-3

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+import ProductDisplay from '@/components/ProductDisplay.vue'
+
+const cart = ref([])
+const premium = ref(true)
+
+const updateCart = (id) => {
+  cart.value.push(id)
+}
+</script>
+  
+<template>
+  <!--navbar-->
+  <div class="nav-bar">
+    <h2>Learn Vue Mastery</h2>
+  </div>
+  <!--cart-->
+  <div class="cart">Cart({{ cart.length }})</div>
+  <ProductDisplay :premium="premium" @add-to-cart="updateCart" @remove-from-cart="removeFromCart"></ProductDisplay>
+</template>
+
+
+
+//import { ref, computed } from 'vue'
+import ReviewForm from '@/components/ReviewForm.vue'
+import ReviewList from '@/components/ReviewList.vue'
+import socksGreenImage from '@/assets/images/socks_green.jpeg'
+import socksBlueImage from '@/assets/images/socks_blue.jpeg'
+import removeFromCart from './components/removeFormCart.vue'
